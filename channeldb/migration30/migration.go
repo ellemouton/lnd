8a9156f65299cd1562c5dfbf5157eff94c129754
Elle Mouton <elle.mouton@gmail.com>
2023-02-02 09:41:56 +0200

    channeldb: add local and remote balance to revocation log
    This commit re-adds the LocalBalance and RemoteBalance fields to the
    RevocationLog. The channeldb/migration30 is also adjusted so that anyone
    who has not yet run the optional migration will not lose these fields if
    they run the migration after this commit.
    
    The reason for re-adding these fields is that they are needed if we want
    to reconstruct all the info of the lnwallet.BreachRetribution without
    having access to the breach spend transaction. In most cases we would
    have access to the spend tx since we would see it on-chain at which time
    we would want to reconstruct the retribution info. However, for the
    watchtower subsystem, we sometimes want to construct the retribution
    info withouth having access to the spend transaction.
    
    A user can use the `--no-rev-log-amt-data` flag to opt-out of storing
    these amount fields.

--- channeldb/migration30/migration.go
+++ channeldb/migration30/migration.go
@@ -553,6 +553,11 @@ func convertRevocationLog(commit *mig.ChannelCommitment,
 		HTLCEntries:      make([]*HTLCEntry, 0, len(commit.Htlcs)),
 	}
 
+	if !noAmtData {
+		rl.TheirBalance = &commit.RemoteBalance
+		rl.OurBalance = &commit.LocalBalance
+	}
+
 	for _, htlc := range commit.Htlcs {
 		// Skip dust HTLCs.
 		if htlc.OutputIndex < 0 {
