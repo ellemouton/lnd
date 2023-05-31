5feb331bdfa091d7337af02713d13441c1f71164
Elle Mouton <elle.mouton@gmail.com>
2023-05-31 08:21:56 +0200

    watchtower/blob: add taproot Commitment type

--- watchtower/blob/type.go
+++ watchtower/blob/type.go
@@ -102,13 +102,18 @@ func (t Type) CommitmentType(chanType *channeldb.ChannelType) (CommitmentType,
 	error) {
 
 	switch {
+	case t.Has(FlagTaprootChannel):
+		return TaprootCommitment, nil
+
 	case t.Has(FlagAnchorChannel):
 		return AnchorCommitment, nil
+
 	case t.Has(FlagCommitOutputs):
 		if chanType != nil && chanType.IsTweakless() {
 			return LegacyTweaklessCommitment, nil
 		}
 		return LegacyCommitment, nil
+
 	default:
 		return 0, ErrUnknownBlobType
 	}
