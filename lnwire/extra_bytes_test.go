aaffa9422b7049f40ba5e5f919d778a29fb25a45
Elle Mouton <elle.mouton@gmail.com>
2023-09-29 09:28:04 +0200

    lnwire: add Encode and Pack methods for tlv.Records

--- lnwire/extra_bytes_test.go
+++ lnwire/extra_bytes_test.go
@@ -118,7 +118,7 @@ func TestExtraOpaqueDataPackUnpackRecords(t *testing.T) {
 	// Now that we have our set of sample records and types, we'll encode
 	// them into the passed ExtraOpaqueData instance.
 	var extraBytes ExtraOpaqueData
-	if err := extraBytes.PackRecords(testRecordsProducers...); err != nil {
+	if err := extraBytes.PackRecordsFromProducers(testRecordsProducers...); err != nil {
 		t.Fatalf("unable to pack records: %v", err)
 	}
 
@@ -128,7 +128,7 @@ func TestExtraOpaqueDataPackUnpackRecords(t *testing.T) {
 		&recordProducer{tlv.MakePrimitiveRecord(type1, &channelType2)},
 		&recordProducer{tlv.MakePrimitiveRecord(type2, &hop2)},
 	}
-	typeMap, err := extraBytes.ExtractRecords(newRecords...)
+	typeMap, err := extraBytes.ExtractRecordsFromProducers(newRecords...)
 	require.NoError(t, err, "unable to extract record")
 
 	// We should find that the new backing values have been populated with
