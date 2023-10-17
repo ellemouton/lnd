9eea29221ccb572914e59d70d709eebe3850ec50
Elle Mouton <elle.mouton@gmail.com>
2023-10-17 16:14:39 +0200

    temp: add ChannelUpdate interface

--- lnwire/interfaces.go
+++ lnwire/interfaces.go
@@ -1,6 +1,11 @@
 package lnwire
 
-import "github.com/btcsuite/btcd/chaincfg/chainhash"
+import (
+	"io"
+
+	"github.com/btcsuite/btcd/chaincfg/chainhash"
+	"github.com/lightningnetwork/lnd/input"
+)
 
 // ChannelAnnouncement is an interface that must be satisfied by any message
 // used to announce and prove the existence of a channel.
@@ -19,6 +24,23 @@ type ChannelAnnouncement interface {
 	// Node2KeyBytes returns the bytes representing the public key of node
 	// 2 in the channel.
 	Node2KeyBytes() [33]byte
+
+	Message
+}
+
+type ChannelUpdate interface { //nolint:interfacebloat
+	Decode(r io.Reader, pver uint32) error
+	SCID() ShortChannelID
+	IsNode1() bool
+	SetDisabled(bool)
+	IsDisabled() bool
+	GetChainHash() chainhash.Hash
+	SetSig(signature input.Signature) error
+	SetSCID(scid ShortChannelID)
+	GetTimeLock() uint16
+	GetBaseFee() MilliSatoshi
+	GetFeeRate() MilliSatoshi
+	GetSignature() Sig
 
 	Message
 }
