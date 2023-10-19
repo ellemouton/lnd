dd8fe5b904da53aba94eee057166133c1732f4ab
Elle Mouton <elle.mouton@gmail.com>
2023-10-19 19:37:13 +0200

    lnwire: let ChannelUpdate2 implement ChannelUpdate

--- lnwire/msg_hash.go
+++ lnwire/msg_hash.go
@@ -1,6 +1,11 @@
 package lnwire
 
-import "github.com/btcsuite/btcd/chaincfg/chainhash"
+import (
+	"bytes"
+	"crypto/sha256"
+
+	"github.com/btcsuite/btcd/chaincfg/chainhash"
+)
 
 const MsgHashTag = "lightning"
 
@@ -10,4 +15,17 @@ func MsgHash(msgName, fieldName string, msg []byte) *chainhash.Hash {
 	tag = append(tag, []byte(fieldName)...)
 
 	return chainhash.TaggedHash(tag, msg)
+}
+
+func MsgHashPreFinalHash(msgName, fieldName string, msg []byte) []byte {
+	tag := []byte(MsgHashTag)
+	shaTag := sha256.Sum256(tag)
+
+	var b bytes.Buffer
+
+	b.Write(shaTag[:])
+	b.Write(shaTag[:])
+	b.Write(msg)
+
+	return b.Bytes()
 }
