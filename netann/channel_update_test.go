dc11825039704a5b7555cc89e9ca7b585eefcc72
Elle Mouton <elle.mouton@gmail.com>
2023-11-07 09:35:14 +0200

    netann: update ChanUpdate modifiers to use interface
    Update the ChannelUpdate modifiers to use the lnwire.ChannelUpdate
    interface instead of *lnwire.ChannelUpdate1.

--- netann/channel_update_test.go
+++ netann/channel_update_test.go
@@ -132,7 +132,7 @@ func TestUpdateDisableFlag(t *testing.T) {
 			err := netann.SignChannelUpdate(
 				tc.signer, testKeyLoc, newUpdate,
 				netann.ChanUpdSetDisable(tc.disable),
-				netann.ChanUpdSetTimestamp,
+				netann.ChanUpdSetTimestamp(0),
 			)
 
 			var fail bool
