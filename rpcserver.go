dc6f7c54d0ff808fec2e7a897ffb8c787f176546
Elle Mouton <elle.mouton@gmail.com>
2024-05-06 16:39:43 +0200

    multi: send to a blinded path in an invoice
    Update the SendPayment flow so that it is able to send to an invoice
    containing a blinded path.

--- rpcserver.go
+++ rpcserver.go
@@ -5120,6 +5120,7 @@ type rpcPaymentIntent struct {
 	paymentAddr        *[32]byte
 	payReq             []byte
 	metadata           []byte
+	blindedPayment     *routing.BlindedPayment
 
 	destCustomRecords record.CustomSet
 
@@ -5254,6 +5255,27 @@ func (r *rpcServer) extractPaymentIntent(rpcPayReq *rpcPaymentRequest) (rpcPayme
 		payIntent.destFeatures = payReq.Features
 		payIntent.paymentAddr = payReq.PaymentAddr
 		payIntent.metadata = payReq.Metadata
+
+		if len(payReq.BlindedPaymentPaths) > 0 {
+			// NOTE: Currently we only choose a single payment path.
+			// This will be updated in a future PR to handle
+			// multiple blinded payment paths.
+			path := payReq.BlindedPaymentPaths[0]
+			finalHop := path.Hops[len(path.Hops)-1]
+			payIntent.blindedPayment =
+				routerrpc.MarshalBlindedPayment(path)
+
+			// Replace the target node with the blinded public key
+			// of the blinded path's final node.
+			copy(
+				payIntent.dest[:],
+				finalHop.BlindedNodePub.SerializeCompressed(),
+			)
+
+			if !payReq.BlindedPaymentPaths[0].Features.IsEmpty() {
+				payIntent.destFeatures = path.Features.Clone()
+			}
+		}
 
 		if err := validateDest(payIntent.dest); err != nil {
 			return payIntent, err
@@ -5409,6 +5431,7 @@ func (r *rpcServer) dispatchPaymentIntent(
 			DestFeatures:       payIntent.destFeatures,
 			PaymentAddr:        payIntent.paymentAddr,
 			Metadata:           payIntent.metadata,
+			BlindedPayment:     payIntent.blindedPayment,
 
 			// Don't enable multi-part payments on the main rpc.
 			// Users need to use routerrpc for that.
