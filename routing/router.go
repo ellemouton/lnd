327d91818c64ceeee367e40d4b1402dc55ae05ce
Elle Mouton <elle.mouton@gmail.com>
2024-06-14 19:51:11 -0400

    multi+refactor: let FetchChanInfos not take tx
    In preparation for having a clean graph DB interface, refactor
    FetchChanInfos so that no transaction can be provided.

--- routing/router.go
+++ routing/router.go
@@ -1022,7 +1022,7 @@ func (r *ChannelRouter) pruneZombieChans() error {
 		}
 
 		disabledEdges, err := r.cfg.Graph.FetchChanInfos(
-			nil, disabledChanIDs,
+			disabledChanIDs,
 		)
 		if err != nil {
 			return fmt.Errorf("unable to fetch disabled channels "+
