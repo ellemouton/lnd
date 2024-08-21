d2858d36d8ac05c35ad202f3494f76070c34c83b
Elle Mouton <elle.mouton@gmail.com>
2024-08-21 11:46:18 +0200

    multi: rename ChannelEdgeInfo
    In preparaion for adding ChannelEdgeInfo2 and a ChannelEdgeInfo
    interface, rename ChannelEdgeInfo to ChannelEdgeInfo1.

--- routing/router_test.go
+++ routing/router_test.go
@@ -2764,7 +2764,7 @@ func TestAddEdgeUnknownVertexes(t *testing.T) {
 	)
 	require.NoError(t, err, "unable to create channel edge")
 
-	edge := &models.ChannelEdgeInfo{
+	edge := &models.ChannelEdgeInfo1{
 		ChannelID:        chanID.ToUint64(),
 		NodeKey1Bytes:    pub1,
 		NodeKey2Bytes:    pub2,
@@ -2843,7 +2843,7 @@ func TestAddEdgeUnknownVertexes(t *testing.T) {
 		10000, 510)
 	require.NoError(t, err, "unable to create channel edge")
 
-	edge = &models.ChannelEdgeInfo{
+	edge = &models.ChannelEdgeInfo1{
 		ChannelID: chanID.ToUint64(),
 		AuthProof: nil,
 	}
