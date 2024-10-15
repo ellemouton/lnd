bb65a9df0ea531100f415a0148c57c60709fedd2
Elle Mouton <elle.mouton@gmail.com>
2024-10-15 14:53:20 +0200

    build: add styling option for console logger

--- build/config.go
+++ build/config.go
@@ -4,8 +4,8 @@ package build
 //
 //nolint:lll
 type LogConfig struct {
-	Console *LoggerConfig `group:"console" namespace:"console" description:"The logger writing to stdout and stderr."`
-	File    *LoggerConfig `group:"file" namespace:"file" description:"The logger writing to LND's standard log file."`
+	Console *consoleLoggerCfg `group:"console" namespace:"console" description:"The logger writing to stdout and stderr."`
+	File    *LoggerConfig     `group:"file" namespace:"file" description:"The logger writing to LND's standard log file."`
 }
 
 // LoggerConfig holds options for a particular logger.
@@ -16,10 +16,19 @@ type LoggerConfig struct {
 	NoTimestamps bool `long:"no-timestamps" description:"Omit timestamps from log lines."`
 }
 
+// consoleLoggerCfg extends the LoggerConfig struct by adding a Color option
+// which is only available for a console logger.
+//
+//nolint:lll
+type consoleLoggerCfg struct {
+	LoggerConfig
+	Style bool `long:"style" description:"If set, the output will be styled with color and fonts"`
+}
+
 // DefaultLogConfig returns the default logging config options.
 func DefaultLogConfig() *LogConfig {
 	return &LogConfig{
-		Console: &LoggerConfig{},
+		Console: &consoleLoggerCfg{},
 		File:    &LoggerConfig{},
 	}
 }
--- build/handlers.go
+++ build/handlers.go
@@ -1,8 +1,11 @@
 package build
 
 import (
+	"fmt"
 	"os"
+	"strings"
 
+	btclogv1 "github.com/btcsuite/btclog"
 	"github.com/btcsuite/btclog/v2"
 )
 
@@ -31,9 +34,73 @@ func NewDefaultLoggers(cfg *LogConfig, rotator *RotatingLogWriter) (
 	if cfg.Console.NoTimestamps {
 		consoleOpts = append(consoleOpts, btclog.WithNoTimestamp())
 	}
+	if cfg.Console.Style {
+		consoleOpts = append(consoleOpts,
+			btclog.WithStyledLevel(
+				func(l btclogv1.Level) string {
+					return styleString(
+						fmt.Sprintf("[%s]", l),
+						boldSeq,
+						string(ansiColoSeq(l)),
+					)
+				},
+			),
+			btclog.WithStyledCallSite(
+				func(file string, line int) string {
+					str := fmt.Sprintf("%s:%d", file, line)
+
+					return styleString(str, faintSeq)
+				},
+			),
+			btclog.WithStyledKeys(func(key string) string {
+				return styleString(key, faintSeq)
+			}),
+		)
+	}
 
 	consoleLogHandler := btclog.NewDefaultHandler(os.Stdout, consoleOpts...)
 	logFileHandler := btclog.NewDefaultHandler(rotator, fileOpts...)
 
 	return consoleLogHandler, logFileHandler
+}
+
+func styleString(s string, styles ...string) string {
+	if len(styles) == 0 {
+		return s
+	}
+
+	seq := strings.Join(styles, ";")
+	if seq == "" {
+		return s
+	}
+
+	return fmt.Sprintf("%s%sm%s%sm", csi, seq, s, csi+resetSeq)
+}
+
+type ansiColorSeq string
+
+const (
+	ansiColorSeqDarkTeal  ansiColorSeq = "38;5;30"
+	ansiColorSeqDarkBlue  ansiColorSeq = "38;5;63"
+	ansiColorSeqLightBlue ansiColorSeq = "38;5;86"
+	ansiColorSeqYellow    ansiColorSeq = "38;5;192"
+	ansiColorSeqRed       ansiColorSeq = "38;5;204"
+	ansiColorSeqPink      ansiColorSeq = "38;5;134"
+)
+
+func ansiColoSeq(l btclogv1.Level) ansiColorSeq {
+	switch l {
+	case btclog.LevelTrace:
+		return ansiColorSeqDarkTeal
+	case btclog.LevelDebug:
+		return ansiColorSeqDarkBlue
+	case btclog.LevelWarn:
+		return ansiColorSeqYellow
+	case btclog.LevelError:
+		return ansiColorSeqRed
+	case btclog.LevelCritical:
+		return ansiColorSeqPink
+	default:
+		return ansiColorSeqLightBlue
+	}
 }
