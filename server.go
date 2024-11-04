86746f953a1defc6c8a3bae005f12007dd2f1110
Elle Mouton <elle.mouton@gmail.com>
2024-11-04 10:17:20 +0200

    multi: add a `--gossip.no-sync` option
    When set, LND will not advertise the gossip queries feature bit and it
    will not initiate gossip syncing with any peer.

--- server.go
+++ server.go
@@ -602,6 +602,7 @@ func newServer(cfg *Config, listenAddrs []net.Addr,
 		NoRouteBlinding:           cfg.ProtocolOptions.NoRouteBlinding(),
 		NoExperimentalEndorsement: cfg.ProtocolOptions.NoExperimentalEndorsement(),
 		NoQuiescence:              cfg.ProtocolOptions.NoQuiescence(),
+		NoGossipQueries:           cfg.Gossip.NoSync,
 	})
 	if err != nil {
 		return nil, err
@@ -4363,6 +4364,7 @@ func (s *server) peerConnected(conn net.Conn, connReq *connmgr.ConnReq,
 				EndorsementExperimentEnd,
 			)
 		},
+		NoGossipSync: s.cfg.Gossip.NoSync,
 	}
 
 	copy(pCfg.PubKeyBytes[:], peerAddr.IdentityKey.SerializeCompressed())
