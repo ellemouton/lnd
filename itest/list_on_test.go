22efe73ba1618f849041ea179d7b35e80947876a
Elle Mouton <elle.mouton@gmail.com>
2024-11-11 10:09:40 +0200

    itest

--- itest/list_on_test.go
+++ itest/list_on_test.go
@@ -702,4 +702,8 @@ var allTestCases = []*lntest.TestCase{
 		Name:     "send to route failed htlc timeout",
 		TestFunc: testSendToRouteFailHTLCTimeout,
 	},
+	{
+		Name:     "remote graph",
+		TestFunc: testRemoteGraph,
+	},
 }
