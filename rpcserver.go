805ab633796b9d25d401ef22f7c3a1612eedde98
Elle Mouton <elle.mouton@gmail.com>
2024-11-12 12:53:53 +0200

    graphrpc: implement an RPC backed GraphSource

--- rpcserver.go
+++ rpcserver.go
@@ -6629,6 +6629,7 @@ func marshalDBEdge(edgeInfo *models.ChannelEdgeInfo,
 		Node2Pub:      hex.EncodeToString(edgeInfo.NodeKey2Bytes[:]),
 		Capacity:      int64(edgeInfo.Capacity),
 		CustomRecords: customRecords,
+		Announced:     edgeInfo.AuthProof != nil,
 	}
 
 	if c1 != nil {
