4b0ff53fab50321639220c74d707dd3809ffa328
Elle Mouton <elle.mouton@gmail.com>
2024-11-12 09:39:13 +0200

    lnrpc: add IsPublic to lnrpc.NodeInfo
    We'll make use of this field later when implementing the rpc version of
    the IsPublic method of the GraphSource interface.

--- rpcserver.go
+++ rpcserver.go
@@ -6785,6 +6785,7 @@ func (r *rpcServer) GetNodeInfo(ctx context.Context,
 		numChannels   uint32
 		totalCapacity btcutil.Amount
 		channels      []*lnrpc.ChannelEdge
+		isPublicNode  bool
 	)
 
 	err = graph.ForEachNodeChannel(ctx, node.PubKeyBytes,
@@ -6793,6 +6794,12 @@ func (r *rpcServer) GetNodeInfo(ctx context.Context,
 
 			numChannels++
 			totalCapacity += edge.Capacity
+
+			// If the edge has an authentication proof, then this
+			// is a public channel and so the node is public.
+			if edge.AuthProof != nil {
+				isPublicNode = true
+			}
 
 			// Only populate the node's channels if the user
 			// requested them.
@@ -6822,6 +6829,7 @@ func (r *rpcServer) GetNodeInfo(ctx context.Context,
 		NumChannels:   numChannels,
 		TotalCapacity: int64(totalCapacity),
 		Channels:      channels,
+		IsPublic:      isPublicNode,
 	}, nil
 }
 
