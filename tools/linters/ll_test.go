146ca93499b0b07ea6976c3e2e2f019a78d3a882
Elle Mouton <elle.mouton@gmail.com>
2024-11-29 11:08:19 +0200

    tools/linters: ignore log lines
    This commit adds a feature to our custom lll linter which will ignore
    log lines (both single and multi-lined log lines) for the lll linter.

--- tools/linters/ll_test.go
+++ tools/linters/ll_test.go
@@ -2,6 +2,7 @@ package linters
 
 import (
 	"os"
+	"regexp"
 	"strings"
 	"testing"
 
@@ -64,15 +65,44 @@ func TestGetLLLIssuesForFile(t *testing.T) {
 		{
 			name: "Long single line log",
 			content: `
-		log.Infof("This is a very long log line but since it is a log line, it should be skipped by the linter.")`,
+			log.Infof("This is a very long log line but since it is a log line, it should be skipped by the linter."),
+			rpcLog.Info("Another long log line with a slightly different name and should still be skipped")`,
+		},
+		{
+			name: "Long single line log followed by a non-log line",
+			content: `
+				log.Infof("This is a very long log line but since it is a log line, it should be skipped by the linter.")
+				fmt.Println("This is a very long line that exceeds the maximum length and should be flagged by the linter.")`,
 			expectedIssue: []string{
-				"the line is 121 characters long, which " +
+				"the line is 140 characters long, which " +
+					"exceeds the maximum of 80 characters.",
+			},
+		},
+		{
+			name: "Multi-line log",
+			content: `
+				log.Infof("This is a very long log line but 
+						since it is a log line, it 
+						should be skipped by the linter.")`,
+		},
+		{
+			name: "Multi-line log followed by a non-log line",
+			content: `
+				log.Infof("This is a very long log line but 
+						since it is a log line, it 
+						should be skipped by the linter.")
+				fmt.Println("This is a very long line that 
+						exceeds the maximum length and 
+						should be flagged by the linter.")`,
+			expectedIssue: []string{
+				"the line is 82 characters long, which " +
 					"exceeds the maximum of 80 characters.",
 			},
 		},
 	}
 
-	tabSpaces := strings.Repeat(" ", 8)
+	tabSpaces := strings.Repeat(" ", defaultTabWidthInSpaces)
+	logRegex := regexp.MustCompile(defaultLogRegex)
 
 	for _, tc := range testCases {
 		t.Run(tc.name, func(t *testing.T) {
@@ -83,7 +113,7 @@ func TestGetLLLIssuesForFile(t *testing.T) {
 
 			// Run the linter on the file.
 			issues, err := getLLLIssuesForFile(
-				tmpFile, 80, tabSpaces,
+				tmpFile, defaultMaxLineLen, tabSpaces, logRegex,
 			)
 			require.NoError(t, err)
 
