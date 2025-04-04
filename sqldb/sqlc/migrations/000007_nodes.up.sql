53bc43d5f4ccaedcd025612ae790d3a6eccd311c
Elle Mouton <elle.mouton@gmail.com>
2025-04-04 09:32:43 +0200

    sqldb: add new graph tables

---
+++ sqldb/sqlc/migrations/000007_nodes.up.sql
@@ -0,0 +1,105 @@
+-- nodes stores all the nodes that we are aware of in the graph
+-- that were gossiped via the V2 gossip protocol.
+CREATE TABLE IF NOT EXISTS nodes (
+    -- The db ID of the node. This will only be used DB level
+    -- relations.
+    id INTEGER PRIMARY KEY,
+
+    -- The protocol version that this node was gossiped on.
+    version SMALLINT NOT NULL,
+
+    -- The public key (serialised compressed) of the node.
+    pub_key BLOB NOT NULL,
+
+    -- The alias of the node.
+    alias VARCHAR,
+
+    -- The signature of the node announcement. If this is null, then
+    -- the node announcement has not been received yet and this record
+    -- is a shell node. This can be the case if we receive a channel
+    -- announcement for a channel that is connected to a node that we
+    -- have not yet received a node announcement for. If the version is
+    -- 1, then this an ECDSA signature. If the version is 2, then this
+    -- is a BIP340 signature.
+    signature BLOB
+);
+CREATE INDEX IF NOT EXISTS nodes_version_idx ON nodes(version);
+-- A node can only have one active node announcement per protocol.
+CREATE UNIQUE INDEX IF NOT EXISTS nodes_unique ON nodes (
+    pub_key, version
+);
+
+-- Any info about the node that is specific to the V1 gossip protocol.
+CREATE TABLE IF NOT EXISTS nodes_v1_data (
+    -- The node id this V1 data belongs to.
+    node_id BIGINT PRIMARY KEY REFERENCES nodes(id) ON DELETE CASCADE,
+
+    -- The unix timestamp of the last time the node was updated.
+    last_update BIGINT NOT NULL,
+
+    -- The color of the node.
+    color VARCHAR NOT NULL
+);
+CREATE INDEX IF NOT EXISTS nodes_v1_data_node_id_last_update_idx
+    ON nodes_v1_data(node_id, last_update);
+
+-- node_extra_types stores any extra TLV fields covered by a node announcement that
+-- we do not have an explicit column for in the nodes table.
+CREATE TABLE IF NOT EXISTS node_extra_types (
+    -- The node id this TLV field belongs to.
+    node_id BIGINT NOT NULL REFERENCES nodes(id) ON DELETE CASCADE,
+
+    -- The Type field.
+    type BIGINT NOT NULL,
+
+    -- The value field.
+    value BLOB
+);
+CREATE INDEX IF NOT EXISTS node_extra_types_node_id_idx ON node_extra_types(node_id);
+CREATE UNIQUE INDEX IF NOT EXISTS node_extra_types_unique ON node_extra_types (
+    type, node_id
+);
+
+CREATE TABLE IF NOT EXISTS features (
+    -- The DB ID of the feature.
+    id INTEGER PRIMARY KEY,
+
+    -- The feature bit value.
+    bit INTEGER NOT NULL UNIQUE
+);
+
+-- node_features contains the feature bits of a node.
+CREATE TABLE IF NOT EXISTS node_features (
+    -- The node id this feature belongs to.
+    node_id BIGINT NOT NULL REFERENCES nodes(id) ON DELETE CASCADE,
+
+    -- The id of the feature that this node has.
+    feature_id BIGINT NOT NULL REFERENCES features(id)
+);
+CREATE INDEX IF NOT EXISTS node_feature_node_id_idx ON node_features(node_id);
+CREATE UNIQUE INDEX IF NOT EXISTS node_features_unique ON node_features (
+    node_id, feature_id
+);
+
+-- node_addresses contains the advertised addresses of nodes.
+CREATE TABLE IF NOT EXISTS node_addresses (
+    -- The node id this feature belongs to.
+    node_id BIGINT NOT NULL REFERENCES nodes(id) ON DELETE CASCADE,
+
+    -- An enum that represents the type of address. This will
+    -- dictate how the address column should be parsed.
+    type SMALLINT NOT NULL,
+
+    position INTEGER NOT NULL, -- ordering within (node_id, type)
+
+    -- The advertised address of the node.
+    address TEXT NOT NULL
+);
+CREATE UNIQUE INDEX IF NOT EXISTS node_addresses_unique ON node_addresses (
+    node_id, type, position
+);
+CREATE INDEX IF NOT EXISTS node_addresses_node_id_idx ON node_addresses(node_id);
+
+CREATE TABLE IF NOT EXISTS  source_nodes(
+    node_id BIGINT PRIMARY KEY REFERENCES nodes (id) ON DELETE CASCADE
+);
---
+++ sqldb/sqlc/migrations/000008_channels.up.sql
@@ -0,0 +1,100 @@
+-- channels stores all teh channels that we are aware of in the graph.
+CREATE TABLE IF NOT EXISTS channels (
+    -- The db ID of the channel.
+    id INTEGER PRIMARY KEY,
+
+    -- The protocol version that this node was gossiped on.
+    version SMALLINT NOT NULL,
+
+    -- The channel id (short channel id) of the channel.
+    scid BLOB NOT NULL,
+
+    node_id_1 BIGINT NOT NULL REFERENCES nodes(id) ON DELETE CASCADE,
+
+    node_id_2 BIGINT NOT NULL REFERENCES nodes(id) ON DELETE CASCADE,
+
+    -- The outpoint of the funding transaction.
+    outpoint TEXT NOT NULL,
+
+    capacity BIGINT NOT NULL
+);
+CREATE INDEX IF NOT EXISTS channels_node_id_1_idx ON channels(node_id_1);
+CREATE INDEX IF NOT EXISTS channels_node_id_2_idx ON channels(node_id_2);
+CREATE UNIQUE INDEX IF NOT EXISTS channels_unique ON channels(version, scid DESC);
+CREATE INDEX IF NOT EXISTS channels_version_outpoint_idx ON channels(version, outpoint);
+
+-- Any info about the channel that is specific to the V1 gossip protocol.
+CREATE TABLE IF NOT EXISTS channels_v1_data (
+    -- The channel id this V1 data belongs to.
+    channel_id BIGINT PRIMARY KEY REFERENCES channels(id) ON DELETE CASCADE,
+
+    bitcoin_key_1 BLOB NOT NULL,
+
+    bitcoin_key_2 BLOB NOT NULL
+);
+
+CREATE TABLE IF NOT EXISTS v1_channel_proofs (
+    channel_id BIGINT PRIMARY KEY REFERENCES channels(id) ON DELETE CASCADE,
+    node_1_signature BLOB NOT NULL,
+    node_2_signature BLOB NOT NULL,
+    bitcoin_1_signature BLOB NOT NULL,
+    bitcoin_2_signature BLOB NOT NULL
+);
+
+-- channel_features contains the feature bits of a channel.
+CREATE TABLE IF NOT EXISTS channel_features (
+    -- The channel id this feature belongs to.
+    channel_id BIGINT NOT NULL REFERENCES channels(id) ON DELETE CASCADE,
+
+    -- The id of the feature that this node has.
+    feature_id BIGINT NOT NULL REFERENCES features(id) ON DELETE CASCADE
+);
+CREATE INDEX IF NOT EXISTS channel_feature_node_id_idx ON channel_features(channel_id);
+CREATE UNIQUE INDEX IF NOT EXISTS channel_features_unique ON channel_features (
+    channel_id, feature_id
+);
+
+-- channel_extra_types stores any extra TLV fields covered by a channels
+-- announcement that we do not have an explicit column for in the channels
+-- table.
+CREATE TABLE IF NOT EXISTS channel_extra_types (
+    -- The channel id this TLV field belongs to.
+    channel_id BIGINT NOT NULL REFERENCES channels(id) ON DELETE CASCADE,
+
+    -- The Type field.
+    type BIGINT NOT NULL,
+
+    -- The value field.
+    value BLOB
+);
+CREATE INDEX IF NOT EXISTS channel_extra_types_channel_id_idx ON channel_extra_types(channel_id);
+CREATE UNIQUE INDEX IF NOT EXISTS channel_extra_types_unique ON channel_extra_types (
+    type, channel_id
+);
+
+CREATE TABLE IF NOT EXISTS zombie_channels (
+    -- The channel id (short channel id) of the channel.
+    scid BIGINT NOT NULL,
+
+    -- The protocol version that this node was gossiped on.
+    version SMALLINT NOT NULL,
+
+    node_key_1 BYTEA,
+    node_key_2 BYTEA
+);
+CREATE UNIQUE INDEX IF NOT EXISTS zombie_channels_channel_id_version_idx
+    ON zombie_channels(scid, version);
+
+
+CREATE TABLE IF NOT EXISTS closed_scids (
+    -- The short channel id of the channel.
+    scid BLOB PRIMARY KEY
+);
+
+CREATE TABLE IF NOT EXISTS prune_log (
+    -- The block height that the prune was performed at.
+    block_height BIGINT PRIMARY KEY,
+
+    -- The block hash that the prune was performed at.
+    block_hash BLOB NOT NULL
+);
