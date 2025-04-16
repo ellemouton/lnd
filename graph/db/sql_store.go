399dca32dddbcb94996b38f5868458ea23417cd8
Elle Mouton <elle.mouton@gmail.com>
2025-04-16 16:13:37 +0200

    ForEachNodeDirectedChannel

--- graph/db/sql_store.go
+++ graph/db/sql_store.go
@@ -1154,6 +1154,113 @@ func (s *SQLStore) ForEachNodeChannel(nodePub route.Vertex,
 	}, func() {})
 }
 
+func (s *SQLStore) ForEachNodeDirectedChannel(nodePub route.Vertex,
+	cb func(channel *DirectedChannel) error) error {
+
+	var (
+		ctx    = context.TODO()
+		readTx = NewReadTx()
+	)
+
+	return s.db.ExecTx(ctx, &readTx, func(db SQLQueries) error {
+		return forEachNodeDirectedChannel(
+			ctx, db, s.cfg.ChainHash, nodePub, cb,
+		)
+	}, func() {})
+}
+
+func forEachNodeDirectedChannel(ctx context.Context, db SQLQueries,
+	chain chainhash.Hash, nodePub route.Vertex,
+	cb func(channel *DirectedChannel) error) error {
+
+	// Fallback that uses the database.
+	toNodeCallback := func() route.Vertex {
+		return nodePub
+	}
+
+	id, err := db.GetNodeIDByPubKeyAndVersion(
+		ctx, sqlc.GetNodeIDByPubKeyAndVersionParams{
+			Version: int16(ProtocolV1),
+			PubKey:  nodePub[:],
+		},
+	)
+	if errors.Is(err, sql.ErrNoRows) {
+		return nil
+	} else if err != nil {
+		return fmt.Errorf("unable to fetch node: %w", err)
+	}
+
+	features, err := getNodeFeatures(ctx, db, id)
+	if err != nil {
+		return fmt.Errorf("unable to fetch node features: %w", err)
+	}
+
+	dbChannels, err := db.ListChannelsByNodeIDAndVersion(
+		ctx, sqlc.ListChannelsByNodeIDAndVersionParams{
+			Version: int16(ProtocolV1),
+			NodeID1: id,
+		},
+	)
+	if err != nil {
+		return fmt.Errorf("unable to fetch channels: %w", err)
+	}
+
+	for _, dbChannel := range dbChannels {
+		e, p1, p2, err := buildChannel(ctx, db, chain, dbChannel)
+		if err != nil {
+			return fmt.Errorf("unable to build channel: %w",
+				err)
+		}
+
+		// Determine the outgoing and incoming policy for this
+		// channel and node combo.
+		outPolicy, inPolicy := p1, p2
+		if p1 != nil && p1.ToNode == nodePub {
+			outPolicy, inPolicy = p2, p1
+		} else if p2 != nil && p2.ToNode != nodePub {
+			outPolicy, inPolicy = p2, p1
+		}
+
+		var cachedInPolicy *models.CachedEdgePolicy
+		if inPolicy != nil {
+			cachedInPolicy = models.NewCachedPolicy(inPolicy)
+			cachedInPolicy.ToNodePubKey = toNodeCallback
+			cachedInPolicy.ToNodeFeatures = features
+		}
+
+		var inboundFee lnwire.Fee
+		if outPolicy != nil {
+			// Extract inbound fee. If there is a decoding
+			// error, skip this edge.
+			_, err := outPolicy.ExtraOpaqueData.
+				ExtractRecords(&inboundFee)
+			if err != nil {
+				return nil
+			}
+		}
+
+		directedChannel := &DirectedChannel{
+			ChannelID:    e.ChannelID,
+			IsNode1:      nodePub == e.NodeKey1Bytes,
+			OtherNode:    e.NodeKey2Bytes,
+			Capacity:     e.Capacity,
+			OutPolicySet: outPolicy != nil,
+			InPolicy:     cachedInPolicy,
+			InboundFee:   inboundFee,
+		}
+
+		if nodePub == e.NodeKey2Bytes {
+			directedChannel.OtherNode = e.NodeKey1Bytes
+		}
+
+		if err := cb(directedChannel); err != nil {
+			return err
+		}
+	}
+
+	return nil
+}
+
 // insertV1Node creates a new V1 node record.
 func insertV1Node(ctx context.Context, db SQLQueries,
 	node *models.LightningNode) (int64, error) {
