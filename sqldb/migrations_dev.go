e1d730e01acab3e6e1d81627b460d2138570da08
Elle Mouton <elle.mouton@gmail.com>
2025-04-17 11:00:30 +0200

    feature flags!

--- sqldb/migrations_dev.go
+++ sqldb/migrations_dev.go
@@ -1,4 +1,4 @@
-//go:build test_db_postgres || test_db_sqlite
+//go:build test_db_postgres || test_db_sqlite || test_native_sql
 
 package sqldb
 
@@ -17,5 +17,14 @@ var migrationAdditions = []MigrationConfig{
 		Name:          "000010_channel_policies",
 		Version:       10,
 		SchemaVersion: 9,
+	},
+	{
+		Name:          "kv_graph_migration",
+		Version:       11,
+		SchemaVersion: 9,
+		// A migration function may be attached to this
+		// migration to migrate KV graph to the native SQL
+		// schema. This is optional and can be disabled by the
+		// user if necessary.
 	},
 }
