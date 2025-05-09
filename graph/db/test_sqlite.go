563f17d504b3894941286491239ed4342e960e99
Elle Mouton <elle.mouton@gmail.com>
2025-05-09 13:04:10 +0200

    graph/db: remove KVStore from SQLStore

--- graph/db/test_sqlite.go
+++ graph/db/test_sqlite.go
@@ -7,21 +7,11 @@ import (
 	"testing"
 
 	"github.com/btcsuite/btcd/chaincfg"
-	"github.com/lightningnetwork/lnd/kvdb"
 	"github.com/lightningnetwork/lnd/sqldb"
-	"github.com/stretchr/testify/require"
 )
 
 // NewTestDB is a helper function that creates an BBolt database for testing.
 func NewTestDB(t testing.TB) *SQLStore {
-	backend, backendCleanup, err := kvdb.GetTestBackend(t.TempDir(), "cgr")
-	require.NoError(t, err)
-
-	t.Cleanup(backendCleanup)
-
-	graphStore, err := NewKVStore(backend)
-	require.NoError(t, err)
-
 	db := sqldb.NewTestSqliteDB(t).BaseDB
 
 	executor := sqldb.NewTransactionExecutor(
@@ -32,6 +22,6 @@ func NewTestDB(t testing.TB) *SQLStore {
 
 	return NewSQLStore(
 		&SQLStoreConfig{ChainHash: *chaincfg.MainNetParams.GenesisHash},
-		executor, graphStore,
+		executor,
 	)
 }
