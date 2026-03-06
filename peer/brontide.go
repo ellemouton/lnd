3fef5bfe7eefa36e3a78324a83fd5ed6cf116877
Elle Mouton <elle.mouton@gmail.com>
2026-03-06 09:13:29 +0200

    multi: add a --gossip.no-sync option
    When set, LND will not advertise the gossip queries feature bit and
    will not initiate gossip syncing with any peer. This is useful when
    LND has been configured with an external graph source (e.g. a remote
    graph) that provides the network topology for pathfinding.
    
    The implementation touches four layers:
      - lncfg: adds the `gossip.no-sync` config option
      - feature: adds `NoGossipQueries` to unset the gossip queries
        feature bits from all feature sets
      - discovery: adds `NoGossipSync` to the gossiper config, causing
        incoming gossip query messages to be rejected with a warning
      - peer: skips gossip syncer initialization when disabled

--- peer/brontide.go
+++ peer/brontide.go
@@ -270,6 +270,10 @@ type Config struct {
 	// gossiper and process remote channel announcements.
 	AuthGossiper *discovery.AuthenticatedGossiper
 
+	// NoGossipSync indicates that gossip syncing has been disabled. When
+	// set, we will not initiate gossip sync with our peers.
+	NoGossipSync bool
+
 	// ChanStatusMgr is used to set or un-set the disabled bit in channel
 	// updates.
 	ChanStatusMgr *netann.ChanStatusManager
@@ -995,6 +999,13 @@ func (p *Brontide) Start() error {
 // initGossipSync initializes either a gossip syncer or an initial routing
 // dump, depending on the negotiated synchronization method.
 func (p *Brontide) initGossipSync() {
+	// If gossip syncing has been disabled, we don't need to initialize a
+	// gossip syncer.
+	if p.cfg.NoGossipSync {
+		p.log.Info("Gossip syncing disabled, skipping init")
+		return
+	}
+
 	// If the remote peer knows of the new gossip queries feature, then
 	// we'll create a new gossipSyncer in the AuthenticatedGossiper for it.
 	if p.remoteFeatures.HasFeature(lnwire.GossipQueriesOptional) {
